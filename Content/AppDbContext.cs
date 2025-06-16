c7d546a12025508f42eeeb4cc3aafddaa523201d
Max <al26538@espa.edu.pt>
2025-06-16 17:47:11 +0100

    commit inicial com projeto Alunos

---
+++ Content/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AlunosApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlunosApi.Content
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+    }
+}
