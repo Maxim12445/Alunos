9e502eeffaa0fef3757f2cb5af356a56d85e6ef4
Max <al26538@espa.edu.pt>
2025-06-17 17:53:29 +0100

    Implementação do padrão Service com IAlunoService, AlunosService e criação do AlunosController (Video 2)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using AlunosApi.Content;
+using AlunosApi.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(x =>
     x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IAlunoService, AlunosService>();
 
 var app = builder.Build();
 
