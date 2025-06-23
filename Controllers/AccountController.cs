a3bcf8b1674b9482743a53c6b0fe7d4005ba5a78
Max <al26538@espa.edu.pt>
2025-06-23 18:13:40 +0100

    Video 13

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,93 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AlunosApi.Services;
+using AlunosApi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AlunosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IAuthenticate _authentication;
+
+        public AccountController(IConfiguration configuration, IAuthenticate authentication)
+        {
+            _configuration = configuration ??
+                throw new ArgumentNullException(nameof(configuration));
+            _authentication = authentication ??
+                throw new ArgumentNullException(nameof(authentication));
+        }
+
+        [HttpPost("createUser")]
+        public async Task<ActionResult<string>> CreateUser([FromBody] RegisterModel model)
+        {
+            if (model.Password != model.ConfirmPassword)
+            {
+                ModelState.AddModelError("ConfirmPassword", "As senhas não conferem");
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authentication.RegisterUser(model.Email, model.Password);
+
+            if (result)
+            {
+                return Ok($"Usuário {model.Email} criado com sucesso");
+            }
+            else
+            {
+                ModelState.AddModelError("CreateUser", "Registo inválido.");
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpPost("loginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.Authenticate(userInfo.Email, userInfo.Password);
+
+            if (result)
+            {
+                return GenerateToken(userInfo);
+            }
+            else
+            {
+                ModelState.AddModelError("LoginUser", "Login inválido");
+                return BadRequest(ModelState);
+            }
+        }
+
+        private UserToken GenerateToken(LoginModel userInfo)
+        {
+            var claims = new[]
+            {
+                new Claim("email", userInfo.Email),
+                new Claim("meuToken", "token do macoratti"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddMinutes(20);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: creds
+            );
+
+            return new UserToken
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,35 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Alunos.API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Cabeçalho de autorização JWT usando o esquema Bearer.\r\n\r\nDigite 'Bearer' [espaço] e depois o token.\r\n\r\nExemplo: Bearer 12345abcdef"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 builder.Services.AddDbContext<AppDbContext>(x =>
     x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
